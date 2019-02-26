0d57207230fe9875d3b6deb57e33c1a8ca658675
Honza Kopecký <honza.kopecky95@gmail.com>
2019-02-26 19:35:00 +0100

    Aggregate user and time

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import csv as csv
 import networkx as net
 import matplotlib.pyplot as plt
 
+from src.aggregate import aggregate
 from src.parser import read_movies, create_graph
 
 def get_top_k_movies(k, recommendations):
@@ -14,22 +15,18 @@ def get_top_k_movies(k, recommendations):
             cnt = cnt + 1
 
 movies = read_movies('data/movies.csv')
-graph = create_graph(movies, 'data/ratings.csv', 1000000000)
+graph = create_graph(movies, 'data/ratings.csv', 10000000000)
+
+graph = aggregate(graph)
 
 # net.draw(graph, pos=net.spring_layout(graph), with_labels=True)
 # plt.show()
-
 preferences = {
     movies[1].label: 1,
     movies[2].label: 1
 }
 
-recommendations = net.pagerank(graph, personalization=preferences)
+recommendations = net.pagerank(graph, personalization=preferences, alpha=0.6)
 recommendations = sorted(recommendations.items(), key=lambda p: p[1], reverse=True)
-
-# print(recommendations[0])
-# print(recommendations[1])
-# print(recommendations[2])
-# print(recommendations[3])
 
 get_top_k_movies(30, recommendations)
