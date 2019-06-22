55d1201ea094a279f3cf2d7c9536719dd6e40891
Honza Kopecký <honza.kopecky95@gmail.com>
2019-06-22 22:19:54 +0200

    Add comments and lecture notes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 import csv as csv
 import networkx as net
 import matplotlib.pyplot as plt
-
 from src.parser import read_movies, create_graph
+
+# NOTES:
+# - recommendation is between information retrieval and information retrieval
+# - both content and queries do vary but quite slowly
+# - recommender system helps to match users with items (books, movies, products, ...)
+# - given the profile of a user and list of items (with description) compute ranking score for each item
+# - content based system, collaborative systems, hybrid systems
+# - represent products and users as nodes in graph
+# - add edges if user rates/buys a product
+# - add edges between similar products
+# - construct adjacency matrix -> transition probability matrix
+# - employ personalized page rank, personalization vector
+
+# LIMITATIONS:
+# - scalability - usually huge graphs
+# - we did not implement aggregation technique in general, instead we have hardcoded aggregation of user and day part
+# - we add edge from event to genre to increase genre importance, other way would be adding weights to different factors
+
+# AGGREGATION:
+# - makes graph smaller, may also improve results if subgraph fits the user needs
+# - two operations - node-edge aggregate and graphs merge
 
 def get_top_k_movies(k, recommendations):
     cnt = 0
@@ -24,12 +44,10 @@ preferences = {
     movies[2].label: 1
 }
 
+# run page rank
+# damping factor is the default: 0.85
 recommendations = net.pagerank(graph, personalization=preferences)
+# sort nodes according to page rank
 recommendations = sorted(recommendations.items(), key=lambda p: p[1], reverse=True)
-
-# print(recommendations[0])
-# print(recommendations[1])
-# print(recommendations[2])
-# print(recommendations[3])
 
 get_top_k_movies(30, recommendations)
